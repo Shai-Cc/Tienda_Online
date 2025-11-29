54d768a32019c37939f57bac8f0dfad646248f95
Shaira Camacho <shaira.0197@gmail.com>
2025-11-29 11:19:10 -0300

    Creación de modelos Productos y Categorías + implementación en admin

--- mainApp/admin.py
+++ mainApp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Categoria, Productos
 
 # Register your models here.
+
+@admin.register(Categoria)
+class CategoriaAdmin (admin.ModelAdmin):
+    list_display = ["nombre",]
+
+@admin.register(Productos)
+class ProductosAdmin (admin.ModelAdmin):
+    list_display = ["nombre",]
--- mainApp/models.py
+++ mainApp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=120, unique=True)
+    slug = models.SlugField(unique=True)
+    descripcion = models.TextField (blank=True)
+
+    def __str__(self):
+        return self.nombre
+
+class Productos(models.Model):
+    nombre = models.CharField(max_length= 120)
+    slug = models.SlugField(unique=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    descripcion = models.TextField()
+    precio_base = models.PositiveIntegerField()
+    imagen1 = models.ImageField(upload_to='productos/')
+    imagen2 = models.ImageField(upload_to='productos/', null=True, blank=True)
+    imagen3 = models.ImageField(upload_to='productos/', null=True, blank=True)
+
+    def __str__(self):
+        return self.nombre
