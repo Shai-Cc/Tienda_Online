646f14a09eb9d266b79304a2dd123310e65d206e
Shaira Camacho <shaira.0197@gmail.com>
2025-11-29 18:20:13 -0300

    Creación de modelo Insumo e implementación del  AdminInsumo

--- mainApp/admin.py
+++ mainApp/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
-from .models import Categoria, Productos
+from .models import Categoria, Productos, Insumo
 
 # Register your models here.
 
 @admin.register(Categoria)
 class CategoriaAdmin (admin.ModelAdmin):
-    list_display = ["nombre",]
+    list_display = ["nombre", "descripcion"]
+    prepopulated_fields = {"slug": ["nombre"]}
 
 @admin.register(Productos)
 class ProductosAdmin (admin.ModelAdmin):
-    list_display = ["nombre",]
+    list_display = ["nombre","slug", "categoria", "descripcion", "precio_base", "imagen1", "imagen2", "imagen3"]
+    prepopulated_fields = {"slug": ["nombre"]}
+
+@admin.register(Insumo)
+class InsumoAdmin (admin.ModelAdmin):
+    list_display = ["nombre", "marca", "color", "cantidad", "unidad"]
--- mainApp/models.py
+++ mainApp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Categoria(models.Model):
     nombre = models.CharField(max_length=120, unique=True)
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(max_length=130, unique=True, null=True)
     descripcion = models.TextField (blank=True)
 
     def __str__(self):
@@ -11,7 +11,7 @@ class Categoria(models.Model):
 
 class Productos(models.Model):
     nombre = models.CharField(max_length= 120)
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(unique=True, max_length=130, null=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
     descripcion = models.TextField()
     precio_base = models.PositiveIntegerField()
@@ -21,3 +21,49 @@ class Productos(models.Model):
 
     def __str__(self):
         return self.nombre
+
+class Insumo(models.Model):
+    nombre = models.CharField(max_length=120)
+    tipo_insumo = [
+        ("textil", "Textil"),
+        ("taza", "Taza"),
+        ("filamento", "Filamento 3D"),
+        ("vinilo", "Vinilo"),
+        ("tinta", "Tinta"),
+        ("papeleria", "Papelería"),
+        ("empaque", "Cajas"),
+        ("otro", "Otro"),
+    ]
+    unidad_insumo = [
+        ("mts", "Metros"),
+        ("cms", "Centímetros"),
+        ("kg", "Kilogramos"),
+        ("gr", "Gramos"),
+        ("Lts", "Litros"),
+        ("pliegos", "Pliegos"),
+        ("cc", "Centímetros Cúbicos"),
+        ("ml", "Mililitros"),
+        ("unidades", "Unidades"),
+        ("otro", "Otro"),
+    ]
+
+    color_insumo = [
+        ("negro", "Negro"),
+        ("amarillo", "Amarillo"),
+        ("azul", "Azul"),
+        ("blanco", "Bolanco"),
+        ("verde", "Verde"),
+        ("rojo", "Rojo"),
+        ("naranja", "Naranja"),
+        ("morado", "Morado"),
+        ("otro", "Otro"),
+    ]
+    tipo = models.CharField(max_length=30, choices=tipo_insumo)
+    cantidad = models.PositiveIntegerField()
+    unidad = models.CharField(max_length= 30, choices=unidad_insumo, blank=True)
+    marca = models.CharField(max_length=100, )
+    color = models.CharField(max_length=20, choices=color_insumo)
+
+    def __str__(self):
+        return self.nombre
+
